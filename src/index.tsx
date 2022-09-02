c270eebcd77d98ef25ae4c2b3202e5f2b69a7573
kzhama <k@zhama.io>
2022-09-02 20:43:04 -0300

    remove chakra ui add mantin ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { MantineProvider } from "@mantine/core";
 
 import App from "./App";
 
@@ -9,8 +9,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<MantineProvider withGlobalStyles withNormalizeCSS>
 			<App />
-		</ChakraProvider>
+		</MantineProvider>
 	</React.StrictMode>
 );
